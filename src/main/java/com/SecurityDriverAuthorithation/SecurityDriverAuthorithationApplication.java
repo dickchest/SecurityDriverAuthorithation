b2caaf2d5c100bbd200ea69e063d034e0de6bdeb
DenysChaykovskyy <dickchest@gmail.com>
2024-01-31 18:05:51 +0100

    Initial commit

---
+++ src/main/java/com/SecurityDriverAuthorithation/SecurityDriverAuthorithationApplication.java
@@ -0,0 +1,13 @@
+package com.SecurityDriverAuthorithation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDriverAuthorithationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDriverAuthorithationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SecurityDriverAuthorithation/SecurityDriverAuthorithationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecurityDriverAuthorithation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDriverAuthorithationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
